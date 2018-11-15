fa316c56204cdfd42a5d70aa2a8423764214c967
Flo Narr <florian.narr@gmail.com>
2018-11-15 07:57:00 +0100

    fix syntax style

--- email.go
+++ email.go
@@ -6,41 +6,41 @@ import (
 )
 
 type SingleSendEmail struct {
-	APIKey     string             `json:"-"`
-	EmailId    int                `json:"emailId"`
-  Message    Message            `json:"message"`
-	ContactProperties []Property  `json:"contactProperties"`
-  CustomProperties  []Property  `json:"customProperties"`
+	APIKey            string     `json:"-"`
+	EmailId           int        `json:"emailId"`
+	Message           Message    `json:"message"`
+	ContactProperties []Property `json:"contactProperties"`
+	CustomProperties  []Property `json:"customProperties"`
 }
 
 type Message struct {
-  To         string             `json:"to"`
-  From       string             `json:"from"`
-  ReplyTo    string             `json:"reply_to"`
+	To      string `json:"to"`
+	From    string `json:"from"`
+	ReplyTo string `json:"reply_to"`
 }
 
 type SingleSendEmailResponse struct {
-	SendResult   string  `json:"send_result"`
-	Message      string  `json:"message"`
-  Id           int     `json:"id"`
+	SendResult string `json:"send_result"`
+	Message    string `json:"message"`
+	Id         int    `json:"id"`
 }
 
 func NewEmail(api_key string, email_id int, message Message) *SingleSendEmail {
 	return &SingleSendEmail{
-		APIKey:   api_key,
-		EmailId:  email_id,
-    Message:  message,
+		APIKey:  api_key,
+		EmailId: email_id,
+		Message: message,
 	}
 }
 
 func (h *SingleSendEmail) Add(prop_type, prop, value string) {
-  if prop_type == "contact" {
-    h.ContactProperties = append(h.ContactProperties, Property{prop, value})
-  }
+	if prop_type == "contact" {
+		h.ContactProperties = append(h.ContactProperties, Property{prop, value})
+	}
 
-  if prop_type == "custom" {
-    h.CustomProperties = append(h.CustomProperties, Property{prop, value})
-  }
+	if prop_type == "custom" {
+		h.CustomProperties = append(h.CustomProperties, Property{prop, value})
+	}
 }
 
 func (h *SingleSendEmail) Publish() (ssr *SingleSendEmailResponse) {
@@ -48,7 +48,7 @@ func (h *SingleSendEmail) Publish() (ssr *SingleSendEmailResponse) {
 
 	b, _ := json.Marshal(h)
 
-  x := Send(url, "POST", b)
+	x := Send(url, "POST", b)
 
 	ssr = &SingleSendEmailResponse{}
 	json.Unmarshal(x, ssr)
